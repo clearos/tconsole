0bf8001b40e135403bebf63dc666a1dcc3b6d92b
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2014-07-24 21:14:47 -0400

    Autoconf clean-up.  Added configure check for ncurses library.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-autoreconf -fi
+find $(pwd) -name configure.ac | xargs touch
+
+# Regenerate configuration files
+mkdir -vp m4 || exit 1
+autoreconf -i --force -I m4 || exit 1
+
